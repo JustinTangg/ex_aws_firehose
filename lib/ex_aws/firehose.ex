aaab20db7d206533b76addbf55472d3454f96385
Justin Tang <justin.tang@vonage.com>
2019-07-24 14:11:34 -0400

    fixing create_delivery_stream

--- lib/ex_aws/firehose.ex
+++ lib/ex_aws/firehose.ex
@@ -15,50 +15,61 @@ defmodule ExAws.Firehose do
 
   @doc "Lists streams"
   @type list_delivery_stream_opts :: [
-    {:exclusive_start_delivery_stream_name, binary} |
-    {:limit, pos_integer}
-  ]
-  @spec list_delivery_streams() :: ExAws.Operation.JSON.t
-  @spec list_delivery_streams(opts :: list_delivery_stream_opts) :: ExAws.Operation.JSON.t
+          {:exclusive_start_delivery_stream_name, binary}
+          | {:limit, pos_integer}
+        ]
+  @spec list_delivery_streams() :: ExAws.Operation.JSON.t()
+  @spec list_delivery_streams(opts :: list_delivery_stream_opts) :: ExAws.Operation.JSON.t()
   def list_delivery_streams(opts \\ []) do
-    data = opts
-    |> camelize_keys
-    |> Map.merge(%{})
+    data =
+      opts
+      |> camelize_keys
+      |> Map.merge(%{})
 
     request(:list_delivery_streams, data)
   end
 
   @doc "Describe Stream"
   @type describe_delivery_stream_opts :: [
-    {:limit, pos_integer} |
-    {:exclusive_start_destination_id, binary}
-  ]
-  @spec describe_delivery_stream(stream_name :: stream_name) :: ExAws.Operation.JSON.t
-  @spec describe_delivery_stream(stream_name :: stream_name, opts :: describe_delivery_stream_opts) :: ExAws.Operation.JSON.t
+          {:limit, pos_integer}
+          | {:exclusive_start_destination_id, binary}
+        ]
+  @spec describe_delivery_stream(stream_name :: stream_name) :: ExAws.Operation.JSON.t()
+  @spec describe_delivery_stream(
+          stream_name :: stream_name,
+          opts :: describe_delivery_stream_opts
+        ) :: ExAws.Operation.JSON.t()
   def describe_delivery_stream(stream_name, opts \\ []) do
-    data = opts
-    |> camelize_keys
-    |> Map.merge(%{"DeliveryStreamName" => stream_name})
+    data =
+      opts
+      |> camelize_keys
+      |> Map.merge(%{"DeliveryStreamName" => stream_name})
+
     request(:describe_delivery_stream, data)
   end
 
   @doc "Creates stream"
   @type create_delivery_stream_opts :: [
-    {:elasticsearch_destination_configuration, Keyword.t} |
-    {:redshift_destination_configuration, Keyword.t} |
-    {:s3_destination_configuration, Keyword.t}
-  ]
-  @spec create_delivery_stream(stream_name :: stream_name) :: ExAws.Operation.JSON.t
-  @spec create_delivery_stream(stream_name :: stream_name, opts :: create_delivery_stream_opts) :: ExAws.Operation.JSON.t
-  def create_delivery_stream(stream_name, shard_count \\ 1) do
-    data = %{
-      "ShardCount" => shard_count,
-      "DeliveryStreamName" => stream_name}
+          {:elasticsearch_destination_configuration, Keyword.t()}
+          | {:redshift_destination_configuration, Keyword.t()}
+          | {:s3_destination_configuration, Keyword.t()}
+        ]
+  @spec create_delivery_stream(stream_name :: stream_name) :: ExAws.Operation.JSON.t()
+  @spec create_delivery_stream(stream_name :: stream_name, opts :: create_delivery_stream_opts) ::
+          ExAws.Operation.JSON.t()
+  def create_delivery_stream(stream_name, opts \\ []) do
+    data =
+      opts
+      |> camelize_keys
+      |> Map.merge(%{
+        "DeliveryStreamName" => stream_name
+      })
+
     request(:create_delivery_stream, data)
   end
 
   @doc "Deletes stream"
-  @spec delete_delivery_stream(stream_name :: stream_name) :: ExAws.Operation.JSON.t
+  @spec delete_delivery_stream(stream_name :: stream_name) :: ExAws.Operation.JSON.t()
   def delete_delivery_stream(stream_name) do
     request(:delete_delivery_stream, %{"DeliveryStreamName" => stream_name})
   end
@@ -67,7 +78,7 @@ defmodule ExAws.Firehose do
   ######################
 
   @doc "Puts a record on a stream"
-  @spec put_record(stream_name :: stream_name, data :: binary) :: ExAws.Operation.JSON.t
+  @spec put_record(stream_name :: stream_name, data :: binary) :: ExAws.Operation.JSON.t()
   def put_record(stream_name, data) do
     data = %{
       "Record" => format_record(data),
@@ -79,12 +90,13 @@ defmodule ExAws.Firehose do
 
   @doc "Put multiple records on a stream"
   @type put_record_batch_record :: [
-    {:data, binary}
-  ]
-  @spec put_record_batch(stream_name :: stream_name, records :: [put_record_batch_record]) :: ExAws.Operation.JSON.t
+          {:data, binary}
+        ]
+  @spec put_record_batch(stream_name :: stream_name, records :: [put_record_batch_record]) ::
+          ExAws.Operation.JSON.t()
   def put_record_batch(stream_name, records) when is_list(records) do
     data = %{
-      "Records"    => records |> Enum.map(&format_record/1),
+      "Records" => records |> Enum.map(&format_record/1),
       "DeliveryStreamName" => stream_name
     }
 
@@ -92,8 +104,9 @@ defmodule ExAws.Firehose do
   end
 
   defp format_record(data) when is_binary(data), do: format_record(%{data: data})
+
   defp format_record(%{data: data}) do
-    %{"Data" => data |> Base.encode64}
+    %{"Data" => data |> Base.encode64()}
   end
 
   ## Destinations
@@ -101,20 +114,30 @@ defmodule ExAws.Firehose do
 
   @doc "Updates the specified destination of the specified delivery stream."
   @type update_destination_opts :: [
-    {:elasticsearch_destination_update, Keyword.t} |
-    {:redshift_destination_update, Keyword.t} |
-    {:s3_destination_update, Keyword.t}
-  ]
-  @spec update_destination(stream_name :: stream_name, version_id :: binary, destination_id :: binary) :: ExAws.Operation.JSON.t
-  @spec update_destination(stream_name :: stream_name, version_id :: binary, destination_id :: binary, opts :: update_destination_opts) :: ExAws.Operation.JSON.t
+          {:elasticsearch_destination_update, Keyword.t()}
+          | {:redshift_destination_update, Keyword.t()}
+          | {:s3_destination_update, Keyword.t()}
+        ]
+  @spec update_destination(
+          stream_name :: stream_name,
+          version_id :: binary,
+          destination_id :: binary
+        ) :: ExAws.Operation.JSON.t()
+  @spec update_destination(
+          stream_name :: stream_name,
+          version_id :: binary,
+          destination_id :: binary,
+          opts :: update_destination_opts
+        ) :: ExAws.Operation.JSON.t()
   def update_destination(stream_name, version_id, destination_id, opts \\ []) do
-    data = opts
-    |> camelize_keys
-    |> Map.merge(%{
-      "DeliveryStreamName" => stream_name,
-      "CurrentDeliveryStreamVersionId" => version_id,
-      "DestinationId" => destination_id,
-    })
+    data =
+      opts
+      |> camelize_keys
+      |> Map.merge(%{
+        "DeliveryStreamName" => stream_name,
+        "CurrentDeliveryStreamVersionId" => version_id,
+        "DestinationId" => destination_id
+      })
 
     request(:update_destination, data)
   end
@@ -122,15 +145,19 @@ defmodule ExAws.Firehose do
   defp request(action, data, opts \\ %{}) do
     operation =
       action
-      |> Atom.to_string
-      |> Macro.camelize
+      |> Atom.to_string()
+      |> Macro.camelize()
 
-    ExAws.Operation.JSON.new(:firehose, %{
-      data: data,
-      headers: [
-        {"x-amz-target", "#{@namespace}.#{operation}"},
-        {"content-type", "application/x-amz-json-1.1"}
-      ]
-    } |> Map.merge(opts))
+    ExAws.Operation.JSON.new(
+      :firehose,
+      %{
+        data: data,
+        headers: [
+          {"x-amz-target", "#{@namespace}.#{operation}"},
+          {"content-type", "application/x-amz-json-1.1"}
+        ]
+      }
+      |> Map.merge(opts)
+    )
   end
 end
